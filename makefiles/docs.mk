ca999b9c990bd065a6f58785aafc0812005718c3
sunakan <sunakan03@gmail.com>
2020-07-25 10:06:33 +0000

    helpドキュメント追加

--- makefiles/docs.mk
+++ makefiles/docs.mk
@@ -5,6 +5,7 @@
 docs: ## documentの生成
 	@mkdir -p ./docs
 	@make --no-print-directory generate-dockerfiles-layout
+	@make --no-print-directory generate-help-docs
 
 
 .PHONY: dockerfiles-layout
@@ -18,3 +19,16 @@ dockerfiles-layout:
 generate-dockerfiles-layout:
 	@make --no-print-directory dockerfiles-layout > ./docs/dockerfiles-layout.adoc
 	@echo Generated: ./docs/dockerfiles-layout.adoc
+
+
+.PHONY: help-docs
+help-docs:
+	@echo '----'
+	@echo '$$ make help'
+	@make --no-print-directory help | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
+	@echo '----'
+
+.PHONY: generate-help-docs
+generate-help-docs:
+	@make --no-print-directory help-docs > ./docs/help.adoc
+	@echo Generated: ./docs/help.adoc
